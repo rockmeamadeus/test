ac867214865261720f8629c902e54e4fd608aa7f
rockmeamadeus <30960639+rockmeamadeus@users.noreply.github.com>
2017-08-13 20:24:13 -0700

    commit inicial v2

---
+++ mutante/src/main/java/mercadolibre/mutante/checker/GraphMutantChecker.java
@@ -0,0 +1,11 @@
+package mercadolibre.mutante.checker;
+
+public class GraphMutantChecker implements IMutantChecker {
+
+	@Override
+	public boolean isMutant(String[] dna) throws Exception {
+
+		throw new UnsupportedOperationException();
+	}
+
+}
